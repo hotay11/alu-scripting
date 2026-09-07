42abffe18a8040683a84740de8e851c14b240438
hotay11 <j.hotay@alustudent.com>
2026-09-07 15:40:14 +0300

    Update 8-textme.rb

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,1 +1,8 @@
-./8-textme.rb 'Feb  1 11:00:01 ip-10-0-0-10 mdr: 2016-02-01 11:00:01 Sent SMS [SMSC:SYBASE1] [SVC:backendtextme] [ACT:] [BINF:] [FID:] [from:14137289325] [to:14136574237] [flags:-1:0:-1:-1:-1] [msg:99:life forms are so amazingly primitive that they still think digital watches are a pretty neat idea.] [udh:0:]'
+cd alu-scripting/regular_expressions
+cat > 8-textme.rb << 'EOF'
+#!/usr/bin/env ruby
+
+log = ARGV[0]
+log.match(/\[from:(.*)\] \[to:(.*)\] \[flags:(.*)\]/)
+puts "#{$1},#{$2},#{$3}"
+EOF

0a56f84bb1f0af493f2be8d7d5639258b99445b8
hotay11 <j.hotay@alustudent.com>
2026-09-07 15:43:40 +0300

    Update 8-textme.rb

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,8 +1,8 @@
-cd alu-scripting/regular_expressions
 cat > 8-textme.rb << 'EOF'
 #!/usr/bin/env ruby
 
 log = ARGV[0]
-log.match(/\[from:(.*)\] \[to:(.*)\] \[flags:(.*)\]/)
+log.match(/\[from:([^\]]*)\] \[to:([^\]]*)\] \[flags:([^\]]*)\]/)
 puts "#{$1},#{$2},#{$3}"
 EOF
+chmod +x 8-textme.rb
